69612a8d162dd2dd9024ad56621545ce575de5d4
Ikadev95 <fegughi@gmail.com>
2024-08-23 15:11:33 +0200

    Es_9_10

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -102,6 +102,11 @@ for (let i = 0; i < cars.length; i++) {
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+let i = 0;
+while (i < numericArray.length && numericArray[i] !== 32) {
+    console.log(numericArray[i]);
+    i++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
@@ -109,3 +114,92 @@ const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+const Positions = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+    let position;
+    switch (charactersArray[i]) {
+        case "a":
+            position = 1;
+            break;
+        case "b":
+            position = 2;
+            break;
+        case "c":
+            position = 3;
+            break;
+        case "d":
+            position = 4;
+            break;
+        case "e":
+            position = 5;
+            break;
+        case "f":
+            position = 6;
+            break;
+        case "g":
+            position = 7;
+            break;
+        case "h":
+            position = 8;
+            break;
+        case "i":
+            position = 9;
+            break;
+        case "j":
+            position = 10;
+            break;
+        case "k":
+            position = 11;
+            break;
+        case "l":
+            position = 12;
+            break;
+        case "m":
+            position = 13;
+            break;
+        case "n":
+            position = 14;
+            break;
+        case "o":
+            position = 15;
+            break;
+        case "p":
+            position = 16;
+            break;
+        case "q":
+            position = 17;
+            break;
+        case "r":
+            position = 18;
+            break;
+        case "s":
+            position = 19;
+            break;
+        case "t":
+            position = 20;
+            break;
+        case "u":
+            position = 21;
+            break;
+        case "v":
+            position = 22;
+            break;
+        case "w":
+            position = 23;
+            break;
+        case "x":
+            position = 24;
+            break;
+        case "y":
+            position = 25;
+            break;
+        case "z":
+            position = 26;
+            break;
+        default:
+            position = -1;
+    }
+    Positions.push(position);
+}
+console.log(Positions);
